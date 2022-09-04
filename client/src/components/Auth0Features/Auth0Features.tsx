9207ede2eea61eae00c7b6acc90d776b2313bc78
mehdi <parastar.mehdi@gmail.com>
2022-09-04 23:39:46 +0430

    starting MUI svg banner

--- client/src/components/Auth0Features/Auth0Features.tsx
+++ client/src/components/Auth0Features/Auth0Features.tsx
@@ -1,86 +1,97 @@
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Grid from '@mui/material/Unstable_Grid2'
-import Auth0BannerSVG from "svg/banners/Auth0";
-import { Auth0Feature } from "./components/Auth0Feature/Auth0Feature";
-
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Unstable_Grid2';
+import Auth0BannerSVG from 'svg/banners/Auth0/Auth0';
+import MUIBannerSVG from 'svg/banners/MUI/MUI';
+import { Auth0Feature } from './components/Auth0Feature/Auth0Feature';
 
 export const Auth0Features: React.FC = () => {
-    const featuresList = [
-        {
-            title: "Auth0 Authentication and Authorization",
-            description:
-                "Auth0 supports social providers such as Google, Facebook, and Twitter, along with Enterprise providers such as Microsoft Office 365, Google Apps, and Azure. You can also use any OAuth 2.0 Authorization Server.",
-            resourceUrl: "https://auth0.com/docs/connections",
-            // banner: "/statics/Auth0Banner.png",
-            banner: <Auth0BannerSVG sx={{
-                backgroundColor: 'whitesmoke',
-                padding: '1rem'
-            }} />,
-        },
-        {
-            title: "Multi-Factor Authentication",
-            description:
-                "You can require your users to provide more than one piece of identifying information when logging in. MFA delivers one-time codes to your users via SMS, voice, email, WebAuthn, and push notifications.",
-            resourceUrl: "https://auth0.com/docs/multifactor-authentication",
-            banner: "https://cdn.auth0.com/blog/hello-auth0/mfa-logo.svg",
-        },
-        {
-            title: "Attack Protection",
-            description:
-                "Auth0 can detect attacks and stop malicious attempts to access your application such as blocking traffic from certain IPs and displaying CAPTCHA. Auth0 supports the principle of layered protection in security that uses a variety of signals to detect and mitigate attacks.",
-            resourceUrl: "https://auth0.com/docs/attack-protection",
-            banner: "https://cdn.auth0.com/blog/hello-auth0/advanced-protection-logo.svg",
-        },
-        {
-            title: "Serverless Extensibility",
-            description:
-                "Actions are functions that allow you to customize the behavior of Auth0. Each action is bound to a specific triggering event on the Auth0 platform. Auth0 invokes the custom code of these Actions when the corresponding triggering event is produced at runtime.",
-            resourceUrl: "https://auth0.com/docs/actions",
-            banner: "https://cdn.auth0.com/blog/hello-auth0/private-cloud-logo.svg",
-        },
-    ];
-    return (
-        <Box
-            sx={{
-                padding: "3.2rem 4.8rem"
-            }}
-        >
-            <Typography
-                variant="h2"
-                sx={{
-                    textAlign: "center",
-                    color: 'white',
-                    fontWeight: 600,
-                    marginTop: 0,
-                    marginBottom: "4.8rem",
-                    fontSize: "3.2rem",
-                    lineHeight: "4.8rem",
-                }}
-            >
-                Explore Outstanding Features
-            </Typography>
-            <Grid
-                container
-                columnSpacing={8}
-                rowSpacing={8}
-                alignItems="center"
-            >
-                {featuresList.map((feature) => (
-                    <Grid
-                        xs={12}
-                        md={6}
-                        key={feature.resourceUrl}
-                    >
-                        <Auth0Feature
-                            title={feature.title}
-                            description={feature.description}
-                            resourceUrl={feature.resourceUrl}
-                            banner={feature.banner}
-                        />
-                    </Grid>
-                ))}
-            </Grid>
-        </Box>
-    )
-}
+  const featuresList = [
+    {
+      title: 'Auth0 Authentication and Authorization',
+      description:
+        'Auth0 supports social providers such as Google, Facebook, and Twitter, along with Enterprise providers such as Microsoft Office 365, Google Apps, and Azure. You can also use any OAuth 2.0 Authorization Server.',
+      resourceUrl: 'https://auth0.com/docs/connections',
+      banner: (
+        <Auth0BannerSVG
+          sx={{
+            backgroundColor: 'whitesmoke',
+            padding: '1rem',
+          }}
+        />
+      ),
+    },
+    {
+      title: 'Variety of Material-UI components',
+      description:
+        'Material-UI is designed for rapid prototyping, increasing overall software development velocity, and creating user interfaces at speed. It helps developers spend more time on the logical layers of their apps rather than wrangling with CSS-related interface elements.',
+      resourceUrl: 'https://mui.com/material-ui/getting-started/overview/',
+      banner: (
+        <MUIBannerSVG
+          sx={{
+            backgroundColor: 'whitesmoke',
+            padding: '1rem',
+          }}
+        />
+      ),
+    },
+    {
+      title: 'Attack Protection',
+      description:
+        'Auth0 can detect attacks and stop malicious attempts to access your application such as blocking traffic from certain IPs and displaying CAPTCHA. Auth0 supports the principle of layered protection in security that uses a variety of signals to detect and mitigate attacks.',
+      resourceUrl: 'https://auth0.com/docs/attack-protection',
+      banner:
+        'https://cdn.auth0.com/blog/hello-auth0/advanced-protection-logo.svg',
+    },
+    {
+      title: 'Serverless Extensibility',
+      description:
+        'Actions are functions that allow you to customize the behavior of Auth0. Each action is bound to a specific triggering event on the Auth0 platform. Auth0 invokes the custom code of these Actions when the corresponding triggering event is produced at runtime.',
+      resourceUrl: 'https://auth0.com/docs/actions',
+      banner: 'https://cdn.auth0.com/blog/hello-auth0/private-cloud-logo.svg',
+    },
+  ];
+  return (
+    <Box
+      sx={{
+        padding: '3.2rem 4.8rem',
+      }}
+    >
+      <Typography
+        variant="h2"
+        sx={{
+          textAlign: 'center',
+          color: 'white',
+          fontWeight: 600,
+          marginTop: 0,
+          marginBottom: '4.8rem',
+          fontSize: '3.2rem',
+          lineHeight: '4.8rem',
+        }}
+      >
+        Explore Outstanding Features
+      </Typography>
+      <Grid
+        container
+        columnSpacing={8}
+        rowSpacing={8}
+        alignItems="center"
+      >
+        {featuresList.map((feature) => (
+          <Grid
+            xs={12}
+            md={6}
+            key={feature.resourceUrl}
+          >
+            <Auth0Feature
+              title={feature.title}
+              description={feature.description}
+              resourceUrl={feature.resourceUrl}
+              banner={feature.banner}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
+};
---
+++ client/src/svg/banners/MUI/MUI.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import { useTheme } from '@mui/material/styles';
+import SvgIcon, { SvgIconProps } from '@mui/material/SvgIcon';
+import { keyframes } from '@emotion/react';
+import { css } from '@emotion/css';
+import { ReactComponent as Svg } from './analytics-laptop-svgrepo-com.svg';
+
+const anim1 = keyframes`
+        0%    { transform: translateY(-48%); }
+        100%  { transform: translateY(0%); }               
+    `;
+const anim2 = keyframes`
+        0%    { transform: translateY(-41%); }
+        100%  { transform: translateY(7%); }              
+    `;
+const anim3 = keyframes`
+        0%    { transform: translateY(-34%); }
+        100%  { transform: translateY(14%); }           
+    `;
+const anim4 = keyframes`
+        0%    { transform: translateY(-27%); }
+        100%  { transform: translateY(21%); }           
+    `;
+const anim5 = keyframes`
+        0%    { transform: translateY(-20%); }
+        100%  { transform: translateY(26%); }           
+    `;
+const anim6 = keyframes`
+        0%    { transform: translateY(-13%); }
+        100%  { transform: translateY(33%); }           
+    `;
+const anim7 = keyframes`
+        0%    { transform: translateY(-6%); }
+        100%  { transform: translateY(40%); }           
+    `;
+const anim8 = keyframes`
+        0%,
+        100% {
+            clip-path: polygon(
+                0% 45%,
+                16% 44%,
+                33% 50%,
+                54% 60%,
+                70% 61%,
+                84% 59%,
+                100% 52%,
+                100% 100%,
+                0% 100%
+            );
+        }
+
+        50% {
+            clip-path: polygon(
+                0% 60%,
+                15% 65%,
+                34% 66%,
+                51% 62%,
+                67% 50%,
+                84% 45%,
+                100% 46%,
+                100% 100%,
+                0% 100%
+            );
+        }  
+    `;
+const anim9 = keyframes`
+        0% {
+            opacity:1
+        }
+        50% {
+            opacity:0
+        }
+        100% {
+            opacity:1
+        }
+    `;
+
+const MUIBannerSVG: React.FC<SvgIconProps> = ({ sx, ...rest }) => {
+  const theme = useTheme();
+  const colorPrimaryLight = theme.palette.primary.light;
+  const colorPrimaryMain = theme.palette.primary.main;
+  const colorPrimaryDark = theme.palette.primary.dark;
+  const colorSecondaryLight = theme.palette.secondary.light;
+  const colorSecondaryMain = theme.palette.secondary.main;
+  const colorSecondaryDark = theme.palette.secondary.dark;
+
+  return (
+    <SvgIcon
+      viewBox="0 0 800 310"
+      sx={{
+        width: '-webkit-fill-available',
+        height: 'auto',
+        ...sx,
+      }}
+      {...rest}
+    >
+      <Svg />
+    </SvgIcon>
+  );
+};
+
+export default MUIBannerSVG;
