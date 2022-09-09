e3820cff9b052fa56065a835049df86e422adfd5
mehdi <parastar.mehdi@gmail.com>
2022-09-09 09:34:38 +0430

    docker svg banner completed

--- client/src/svg/banners/Docker/Docker.tsx
+++ client/src/svg/banners/Docker/Docker.tsx
@@ -7,28 +7,66 @@ import { ReactComponent as Svg } from './Docker1.svg';
 
 const hook_arm_anim = keyframes`
   0% {transform: scaleY(0);}
-  80% {transform: scaleY(1.2);}
-  85% {transform: scaleY(1.2);}
-  100% {transform: scaleY(0.9);}
+  8.4% {transform: scaleY(1.2);}
+  10.2% {transform: scaleY(1.2);}
+  12% {transform: scaleY(0.9);}
+  27% {transform: scaleY(0.9);}
+  33% {transform: scaleY(1.7);}
+  35% {transform: scaleY(1.5);}
+  50% {transform: scaleY(0);}
+  100% {transform: scaleY(0);}
 `;
 
 const hook_head_anim = keyframes`
       0%    { transform: translateY(-52%); }
-      80%  { transform: translateY(13%); }        
-      85%  { transform: translateY(13%); }        
-      100%  { transform: translateY(-13%); }        
+      8.4%  { transform: translateY(-8%); }        
+      10.2%  { transform: translateY(-8%); }        
+      12%  { transform: translateY(-23%); }
+      27%  { transform: translateY(-23%); }        
+      33%  { transform: translateY(12.2%); }        
+      35%  { transform: translateY(10.2%); }        
+      50%  { transform: translateY(-52%); } 
+      100%  { transform: translateY(-52%); }        
     `;
 
 const crane_arm_and_hook_anim = keyframes`
     0%    { transform-origin: right center; transform: rotate3d(0,0,0,0deg); }          
-    100%  { transform-origin: 89% center; transform: rotate3d(0,1,0,40deg); }        
+    12%  { transform-origin: 89% center; transform: rotate3d(0,1,0,35deg); }        
+    23%  { transform-origin: 89% center; transform: rotate3d(0,1,0,35deg); } 
+    38% { transform-origin: 89% center; transform: rotate3d(0,1,0,0deg); }
+    100%  { transform-origin: 89% center; transform: rotate3d(0,1,0,0deg); }            
   `;
 const docker_anim = keyframes`
       0%    { transform: translate(-30%, 18.5%); }
-      20%    { transform: translate(40%, 18.5%); }
-      80%    { transform: translate(40%, 18.5%); }
+      47%    { transform: translate(42%, 18.5%); }
+      55%    { transform: translate(42%, 18.5%); }
+      70%    { transform: translate(110%, 18.5%); }
       100%  { transform: translate(110%, 18.5%); }  
     `;
+const truck_anim = keyframes`
+    0%   { transform: translateX(3000px); }
+    20%  { transform: translateX(0px); }
+    34%  { transform: translateX(0px); }
+    52%  { transform: translateX(-3500px); }
+    100% { transform: translateX(-3500px); }
+  `;
+
+const containers_anim = keyframes`
+  0%   { transform: translateX(535px); }
+  20%  { transform: translateX(106px); }
+  30%  { transform: translateX(106px); }
+  32.4%  { transform: translate(106px,-14.5px); }
+  33.4%  { transform: translate(106px,-14.5px); }
+  37.2%  { transform: translate(114px,-14.5px); }
+  38.34%  { transform: translate(117px,-14.5px); }
+  41%  { transform: translate(124px,-14.5px); }
+  44%  { transform: translate(139px,-14.5px); }
+  47%  { transform: translate(139px,-14.5px); }
+  53%  { transform: translate(139px,20px) }
+  55%  { transform: translate(139px,20px) }
+  70%  { transform: translate(680px,20px) }
+  100% { transform: translate(680px,20px); }
+`;
 const sea_waves_anim = keyframes`
       0%    { transform: translate(50%);}
       100%  { transform: translate(-50%);}  
@@ -73,58 +111,21 @@ const clouds_anim06 = keyframes`
       95.0001%  { transform: translate(100%);}          
       100%  { transform: translate(0%);}          
     `;
-const anim7 = keyframes`
-        0%    { transform: translateY(-6%); }
-        100%  { transform: translateY(40%); }           
-    `;
-const anim8 = keyframes`
-        0%,
-        100% {
-            clip-path: polygon(
-                0% 45%,
-                16% 44%,
-                33% 50%,
-                54% 60%,
-                70% 61%,
-                84% 59%,
-                100% 52%,
-                100% 100%,
-                0% 100%
-            );
-        }
 
-        50% {
-            clip-path: polygon(
-                0% 60%,
-                15% 65%,
-                34% 66%,
-                51% 62%,
-                67% 50%,
-                84% 45%,
-                100% 46%,
-                100% 100%,
-                0% 100%
-            );
-        }  
+const text_anim = keyframes`    
+      0%{
+        stroke-dasharray: 0;
+      }
+      15%{
+        stroke-dasharray: 20;
+      }
+      30%{
+        stroke-dasharray: 0;
+      }
+      100%{
+        stroke-dasharray: 0;
+      }
     `;
-const anim9 = keyframes`
-        0% {
-            opacity:1
-        }
-        50% {
-            opacity:0
-        }
-        100% {
-            opacity:1
-        }
-    `;
-
-function getTime(seconds: number): number {
-  const msDuration: number = (seconds * 1000);
-  const currentTime: number = new Date().getTime();
-  const msDelay: number = msDuration - (currentTime % msDuration);
-  return (msDelay / 1000);
-}
 
 const DockerBannerSVG: React.FC<SvgIconProps> = ({ sx, ...rest }) => {
   const theme = useTheme();
@@ -151,38 +152,92 @@ const DockerBannerSVG: React.FC<SvgIconProps> = ({ sx, ...rest }) => {
           #hook_arm {
             transform: scaleY(0);
             transform-origin: center center;
-            animation: 6s ${hook_arm_anim} linear forwards;
+            animation: 35s ${hook_arm_anim} 7s linear infinite forwards;
           }
 
           #hook_head {
-            animation: 6s ${hook_head_anim} linear forwards;
+            transform: translateY(-52%);
+            animation: 35s ${hook_head_anim} 7s linear infinite forwards;
           }
-          
+
           #crane_arm_and_hook {
-            animation: 6s ${crane_arm_and_hook_anim} linear forwards;
+            animation: 35s ${crane_arm_and_hook_anim} 11.2s linear infinite
+              forwards;
           }
 
-          #docker {animation: 20s ${docker_anim} linear infinite;}
-          
-          #first_wave {animation: 480s ${sea_waves_anim} 0s linear infinite both;}
-          #first_wave_ {animation: 480s ${sea_waves__anim} 0s linear infinite both;}
-          
-          #second_wave {animation: 480s ${sea_waves_anim} 0s linear infinite both;}
-          #second_wave_ {animation: 480s ${sea_waves__anim} 0s linear infinite both;}
-          
-          #third_wave {animation: 480s ${sea_waves_anim} 0s linear infinite both;}
-          #third_wave_ {animation: 480s ${sea_waves__anim} 0s linear infinite both;}
-                  
-          #forth_wave {animation: 480s ${sea_waves_anim} 0s linear infinite both;}
-          #forth_wave_ {animation: 480s ${sea_waves__anim} 0s linear infinite both;}    
-          
-          #cloud01 {animation: 240s ${clouds_anim01} 0s linear infinite forwards;}
-          #cloud02 {animation: 240s ${clouds_anim02} 0s linear infinite forwards;}
-          #cloud03 {animation: 240s ${clouds_anim03} 0s linear infinite forwards;}
-          #cloud04 {animation: 240s ${clouds_anim04} 0s linear infinite forwards;}
-          #cloud05 {animation: 240s ${clouds_anim05} 0s linear infinite forwards;}
-          #cloud06 {animation: 240s ${clouds_anim06} 0s linear infinite forwards;}
+          #docker {
+            animation: 35s ${docker_anim} linear infinite forwards;
+          }
+          #truck {
+            transform-origin: 0px 0px;
+            animation-name: ${truck_anim};
+            animation-delay: 0s;
+            animation-duration: 35s;
+            animation-fill-mode: forwards;
+            animation-timing-function: linear;
+            animation-iteration-count: infinite;
+          }
+          #containers {
+            transform-origin: 0px 0px;
+            animation-name: ${containers_anim};
+            animation-delay: 0s;
+            animation-duration: 35s;
+            animation-fill-mode: forwards;
+            animation-timing-function: linear;
+            animation-iteration-count: infinite;
+          }
 
+          #text {
+            font-family: 'Yellowtail' !important;
+            font-size: xxx-large !important;
+            animation: 35s ${text_anim} 24.5s linear infinite forwards;
+          }          
+
+          #first_wave {
+            animation: 480s ${sea_waves_anim} 0s linear infinite both;
+          }
+          #first_wave_ {
+            animation: 480s ${sea_waves__anim} 0s linear infinite both;
+          }
+          #second_wave {
+            animation: 480s ${sea_waves_anim} 0s linear infinite both;
+          }
+          #second_wave_ {
+            animation: 480s ${sea_waves__anim} 0s linear infinite both;
+          }
+          #third_wave {
+            animation: 480s ${sea_waves_anim} 0s linear infinite both;
+          }
+          #third_wave_ {
+            animation: 480s ${sea_waves__anim} 0s linear infinite both;
+          }
+          #forth_wave {
+            animation: 480s ${sea_waves_anim} 0s linear infinite both;
+          }
+          #forth_wave_ {
+            animation: 480s ${sea_waves__anim} 0s linear infinite both;
+          }
+
+          #cloud01 {
+            animation: 240s ${clouds_anim01} 0s linear infinite forwards;
+          }
+          #cloud02 {
+            animation: 240s ${clouds_anim02} 0s linear infinite forwards;
+          }
+          #cloud03 {
+            animation: 240s ${clouds_anim03} 0s linear infinite forwards;
+          }
+          #cloud04 {
+            animation: 240s ${clouds_anim04} 0s linear infinite forwards;
+          }
+          #cloud05 {
+            animation: 240s ${clouds_anim05} 0s linear infinite forwards;
+          }
+          #cloud06 {
+            animation: 240s ${clouds_anim06} 0s linear infinite forwards;
+          }
+
+          
         `}
       />
     </SvgIcon>
