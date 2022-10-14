dab13aea95da27f428ca4775fa75cb3e7bdf95b2
mehdi <parastar.mehdi@gmail.com>
2022-10-14 13:32:51 +0330

    ssl added to linux client and server recreated

--- servers/crud-app/src/blog/dto/update-post.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePostDTO } from './create-post.dto';
-
-export class UpdatePostDTO extends PartialType(CreatePostDTO) {}
--- servers/crud-app/src/blog/interfaces/post.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from 'mongoose';
-
-export interface IPost extends Document {
-  readonly title: string;
-  readonly description: string;
-  readonly body: string;
-  readonly author: string;
-  readonly date_posted: string;
-}
